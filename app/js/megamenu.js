1181603456dd12b337b620fa99d2e5704841342a
William James <williamjames.developer@gmail.com>
2021-11-13 07:54:19 -0800

    added layout for header partial

---
+++ app/js/megamenu.js
@@ -0,0 +1,87 @@
+/*global $ */
+$(document).ready(function () {
+
+    "use strict";
+
+    // Checks if li has sub (ul) and adds class for toggle icon - just an UI
+    $('.menu > ul > li:has( > .mega-menu), .menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+
+    // Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
+    $('.menu > ul > li > .mega-menu:not(:has(ul)), .menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+
+    // Adds menu-mobile class (for mobile toggle menu) before the normal menu
+    // Mobile menu is hidden if width is more then 992px, but normal menu is displayed
+    // Normal menu is hidden if width is below 992px, and jquery adds mobile menu
+    // Done this way so it can be used with wordpress without any trouble
+    $(".menu > ul").before("<a class=\"menu-mobile\" data-toggle=\"collapse\" data-target=\"#collapseMenu\" aria-expanded=\"false\" aria-controls=\"collapseMenu\">Navigation</a>");
+
+    // If width is more than 992px dropdowns are displayed on hover
+    $(".menu > ul > li").hover(
+        function (e) {
+            if ($(window).outerWidth() > 992) {
+                $(this).children(".mega-menu").fadeIn(150);
+                $(this).children("ul").fadeIn(150);
+                e.preventDefault();
+            }
+        }, function (e) {
+            if ($(window).outerWidth() > 992) {
+                $(this).children(".mega-menu").fadeOut(150);
+                $(this).children("ul").fadeOut(150);
+                e.preventDefault();
+            }
+        }
+    );
+
+    // The following hides the menu when a click is registered outside
+    $(document).on('click', function(e){
+        // if($(e.target).parents('.menu').length === 0)
+        //     $(".menu > ul").removeClass('show-on-mobile');
+    });
+
+    $(".menu > ul > li").click(function() {
+        // no more overlapping menus
+        // hides other children menus when a list item with children menus is clicked
+        var thisMenu = $(this).children("ul");
+        var thisMegaMenu = $(this).children(".mega-menu");
+        
+    // Don't need this function right now
+    //        
+    //         if(thisMenu) {
+    //             var prevState = thisMenu.css('display');
+    //             thisMenu.fadeOut();
+    //             $('.menu-dropdown-icon.clicked').removeClass('clicked');
+    //             if ($(window).outerWidth() < 992) {
+    //                 if(prevState !== 'block') { 
+    //                     thisMenu.fadeIn(150); 
+    //                     $('.mega-menu').not(thisMenu).hide();
+    //                     thisMenu.parent('.menu-dropdown-icon').addClass('clicked'); 
+    //                 } else {
+    //                     thisMenu.parent('.menu-dropdown-icon.clicked').removeClass('clicked');
+    //                 }
+    //                   
+    //             }
+    //         }
+
+        // If width is less or equal to 992px dropdowns (BS4 Medium size)
+        if (thisMegaMenu) {
+            var prevState = thisMegaMenu.css('display');
+            thisMegaMenu.fadeOut();
+            $('.menu-dropdown-icon.clicked').removeClass('clicked');
+            if ($(window).outerWidth() < 992) {
+                if(prevState !== 'block') { 
+                    thisMegaMenu.fadeIn(150); 
+                    $('.mega-menu').not(thisMegaMenu).hide();
+                    thisMegaMenu.parent('.menu-dropdown-icon').addClass('clicked'); 
+                } else {
+                    thisMegaMenu.parent('.menu-dropdown-icon.clicked').removeClass('clicked');
+                }
+            }
+        }
+    });
+
+    // When clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
+    $(".menu-mobile").click(function (e) {
+        $(".menu > ul").toggleClass('show-on-mobile');
+        e.preventDefault();
+    });
+});
